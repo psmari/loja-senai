da8969dd697d74839e5ffc5c4868210971c6d5f1
psmari <marips0202@gmail.com>
2025-10-28 19:06:41 -0300

    adicionei o customer, criando seu módulo

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
+import { CustomersModule } from './customers/customers.module';
 
 @Module({
-  imports: [ProductsModule],
+  imports: [ProductsModule, CustomersModule],
   controllers: [AppController],
   providers: [AppService],
 })
