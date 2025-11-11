3c90e8bc4a655014f1c11614c75d237f5347e137
psmari <marips0202@gmail.com>
2025-11-10 21:51:21 -0300

    Conecta no banco

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,22 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 import { CustomersModule } from './customers/customers.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ProductsModule, CustomersModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306, // padrão
+      username: 'root',
+      password: '',
+      database: 'curso',
+      entities: [],
+      synchronize: false,  // importante! false em produção
+      logging: true,
+    }),
+    ProductsModule, CustomersModule],
   controllers: [AppController],
   providers: [AppService],
 })
